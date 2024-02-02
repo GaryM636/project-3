76cac7352ea64007aa827a93b284a095e47154c7
francis <clyderitchie@yahoo.com>
2024-02-01 21:38:54 -0800

    Added the queries and mutations

---
+++ client/src/utils/api.js
@@ -0,0 +1,57 @@
+// User 
+export const getAllUsers = () => {
+    return fetch('/api/user', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+  
+  export const getUser = (userId) => {
+    return fetch(`/api/user/${userId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+  
+  export const createUser = (userData) => {
+    return fetch('/api/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+  };
+
+// Post
+export const getAllPosts = () => {
+    return fetch('/api/post', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+  
+  export const getPost = (postId) => {
+    return fetch(`/api/post/${postId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+  
+  export const createPost = (postData) => {
+    return fetch('/api/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postData),
+    });
+  };
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,34 @@
+import { gql } from '@apollo/client';
+
+// User
+export const CREATE_USER = gql`
+mutation createUser($username: String, $password: String, $email: String) {
+  createUser(username: $username, password: $password, email: $email) {
+    token
+  }
+}
+`;
+
+// Post
+export const CREATE_POST = gql`
+mutation CreatePost($text: String) {
+  createPost(text: $text) {
+    text
+    userId {
+      username
+    }
+  }
+}
+`;
+
+// Login
+export const LOGIN = gql`
+mutation login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+    }
+  }
+}
+`;
