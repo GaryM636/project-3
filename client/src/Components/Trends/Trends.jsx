ebd16ed32593a5a310667cb635071b8c9376be56
GaryM636 <142124124+GaryM636@users.noreply.github.com>
2024-02-05 18:38:11 -0800

    added a skeleton of menu and app layout

---
+++ client/src/Components/Trends/Trends.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Trends() {
+    return (
+        <>
+          <div className="trends-section">
+            <h1>Trends</h1>
+          </div>
+        </>
+    );
+}
+
+export default Trends;
