065a9f450aca095d65de6feceebcd231c496efaa
GaryM636 <142124124+GaryM636@users.noreply.github.com>
2024-02-08 18:40:04 -0800

    cards for post with and without pictures, minor css

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import PostWithImg from "../Components/SinglePost/PostWithImg.jsx";
+import PostWoImg from "../Components/SinglePost/PostWoImg.jsx";
 
 function Home() {
     return (
         <>
             <h1>HOME</h1>
+            <div className="home-render">
+            <PostWithImg />
+            <PostWoImg />
+            </div>
         </>
     );
 };
