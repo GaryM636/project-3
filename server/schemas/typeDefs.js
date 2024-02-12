8b1bd30d5595ab478e0cca41bcd55b6dafa13e68
Chris Waje <chriswaje324@gmail.com>
2024-02-12 13:08:16 -0800

    FIXED: features in signup, queries, and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -19,6 +19,7 @@ module.exports = gql`
         text: String
         picture: String
         userId: User
+        createdAt: String
         likes: [PostLikes]
         comments: [Comment]
     }
