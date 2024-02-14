335acdc6b62cf5c37df13f91ed00bae151358937
GaryM636 <142124124+GaryM636@users.noreply.github.com>
2024-02-13 18:48:11 -0800

    deploying 3

--- server/server.js
+++ server/server.js
@@ -23,6 +23,13 @@ const startApolloServer = async () => {
 	app.use('/graphql', expressMiddleware(server, {
 		context: authMiddleware
 	}));
+	if (process.env.NODE_ENV === 'production') {
+		app.use(express.static(path.join(__dirname, '../client/dist')));
+	
+		app.get('*', (req, res) => {
+		  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+		});
+	  }
 
 	db.once('open', () => {
 		app.listen(PORT, () => {
